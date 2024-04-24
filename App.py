6808ce705d23412a93dd54a373401ec71cbd036d
Valdeison Lima <valdeilsonbdl56@gmail.com>
2024-04-23 22:25:39 -0300

    Update App.py

--- App.py
+++ App.py
@@ -21,22 +21,31 @@ def finalizar_app():
       os.system('cls')
       print('Finalizando o app\n')
 
+def opçãoInvalida():
+      print('Opção invalida\n')
+      input('Digite uma tecla para voltar ao menu principal')
 
 
 def escolheropcao():
-      opcaoEscolhida = int(input('Escolha uma opção: '))
-      # opcaoEscolhida = int(opcaoEscolhida)
+      try:
+            opcaoEscolhida = int(input('Escolha uma opção: '))
+            # opcaoEscolhida = int(opcaoEscolhida)
 
-      if opcaoEscolhida == 1:
-            print('Cadastrar restaurante')
-      elif opcaoEscolhida == 2:
-            print('LIstar restaurantes')
-      elif opcaoEscolhida == 3:
-            print('Ativar restaurante')
-      else:
-            finalizar_app()
+            if opcaoEscolhida == 1:
+                  print('Cadastrar restaurante')
+            elif opcaoEscolhida == 2:
+                  print('LIstar restaurantes')
+            elif opcaoEscolhida == 3:
+                  print('Ativar restaurante')
+            elif opcaoEscolhida == 4:
+                  finalizar_app()
+            else:
+                  opçãoInvalida()
+      except:   
+            opçãoInvalida()        
 
 def main():
+      os.system('cls')
       exibirnomedoprograma()
       exibir_opcoes()
       escolheropcao()
